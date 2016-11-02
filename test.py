8ebc1f8bc215833efa321a2428f9c5da16ec3046
1sForTheElder <s1668779@sms.ed.ac.uk>
2016-11-02 02:22:35 +0000

    first

---
+++ test.py
@@ -0,0 +1,16 @@
+import nltk
+import sys,re
+import nltk
+from collections import defaultdict
+import cfg_fix
+from cfg_fix import parse_grammar, CFG
+from pprint import pprint
+# The printing and tracing functionality is in a separate file in order
+#  to make this file easier to read
+from cky_print import CKY_pprint, CKY_log, Cell__str__, Cell_str, Cell_log
+
+
+mylist = {"123","432","235","sdf"}
+mylist.pop()
+mylist.pop()
+print mylist
